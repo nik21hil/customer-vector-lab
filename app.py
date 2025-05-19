e83fac2f5c2cb5456b38a3f3c39911b557967691
Nikhil Singh <nik21hil@gmail.com>
2025-05-19 20:58:15 +0000

    Final layout polish: apply button, chart sizing, column layout

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ from src.visualize import plot_umap, plot_radar_chart, plot_cluster_distribution
 
 st.set_page_config(page_title="Customer Vector Lab")
 
-st.markdown("<h1 style='text-align: center;'>🧩 Customer Vector Lab</h1>", unsafe_allow_html=True)
+st.markdown("<h1 style='text-align: center;'>🧩 Customer Vector Lab 🧩</h1>", unsafe_allow_html=True)
 
 st.markdown("""
 <div style='text-align: center; font-size:16px;'>
